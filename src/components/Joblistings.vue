8c8b26156a1383a3f79436158250d559caba8fab
Vineet Kumar Verma <vineetkverma1947@gmail.com>
2024-10-05 16:51:13 +0530

    job rendring

--- src/components/Joblistings.vue
+++ src/components/Joblistings.vue
@@ -2,17 +2,13 @@
 import { ref } from 'vue'
 import jobData from '@/jobs.json'
 
-const jobs = ref(jobData)
+const jobs = ref(jobData.jobs)
 </script>
 
 <template>
-    Job listing
-    <div v-for="job in jobs.value" :key="job.id">
+    <h1>Job Listings</h1>
+
+    <div v-for="job in jobs" :key="job.id">
         {{ job.title }}
     </div>
-    <!-- <section class="bg-blue-50 px-4 py-10">
-        <div v-for="job in jobs" :key="job.id">
-            {{ job.title }}
-        </div>
-    </section> -->
 </template>
